06088cbdad8ed7efe102f999563077cf62dcf48d
Parsa Abdollahi <parsa.abdollahi.pa@gmail.com>
2021-09-26 15:16:34 +0330

    Refactor Dinic and Dinic with low scaling

--- dinic with low scaling.cpp
+++ dinic with low scaling.cpp
@@ -1,63 +1,57 @@
-// MaxFlow Dinic algorithm with scaling. 
-// O(N * M * log(MC)), where MC is maximum edge capacity. 
-// Based on problem 2784 from informatics.mccme.ru 
-// http://informatics.mccme.ru/mod/statements/view3.php?chapterid=2784#1
-#include <bits/stdc++.h>
-using namespace std;
-struct edge {
-    int a, b, f, c;
+// MaxFlow Dinic algorithm with scaling.
+// O(N * M * log(MC)), where MC is maximum edge capacity.
+// Based on problem http://informatics.mccme.ru/mod/statements/view3.php?chapterid=2784#1
+// For not using long long, make all "ll"s int and change infs.
+struct Edge {
+    int a, b;
+    ll f, c;
+    Edge (int a, int b, ll f, ll c) : a(a), b(b), f(f), c(c) {};
 };
-const int inf = 1000 * 1000 * 1000;
-const int MAXN = 1050;
-int n, m;
-vector <edge> e;
+const int INF_BFS=1e9;
+const int MAXN = 550;
+const ll INF_CAP = (ll)1e16;
+int d[MAXN], source=MAXN-2, sink=MAXN-1;
 int pt[MAXN]; // very important performance trick
-vector <int> g[MAXN];
-long long flow = 0;
-queue <int> q;
-int d[MAXN];
-int lim;
-int s, t;
+vector <Edge> e;
+vector <ll> g[MAXN];
+ll lim;
 
-void add_edge(int a, int b, int c) {                                                                           
-    edge ed;
-    //keep edges in vector: e[ind] - direct edge, e[ind ^ 1] - back edge 
-    ed.a = a; ed.b = b; ed.f = 0; ed.c = c;
-    g[a].push_back(e.size());
-    e.push_back(ed);
+void add_edge(int a, int b, ll ab_cap, ll ba_cap=0) {
+    //keep edges in vector: e[ind] - direct edge, e[ind ^ 1] - back edge
+    g[a].emplace_back(e.size());
+    e.emplace_back(Edge(a, b, 0, ab_cap));
 
-    ed.a = b; ed.b = a; ed.f = c; ed.c = c;
-    g[b].push_back(e.size());
-    e.push_back(ed);
+    g[b].emplace_back(e.size());
+    e.emplace_back(Edge(b, a, 0, ba_cap));
 }
 
 bool bfs() {
-    for (int i = s; i <= t; i++)
-        d[i] = inf;
-    d[s] = 0; 
-    q.push(s);
-    while (!q.empty() && d[t] == inf) {
+    fill(d, d+MAXN, INF_BFS); // be cautious about using this.
+    d[source] = 0;
+    queue <int> q;
+    q.push(source);
+    while (!q.empty() && d[sink] == INF_BFS) {
         int cur = q.front(); q.pop();
         for (size_t i = 0; i < g[cur].size(); i++) {
             int id = g[cur][i];
             int to = e[id].b;
             //printf("cur = %d id = %d a = %d b = %d f = %d c = %d\n", cur, id, e[id].a, e[id].b, e[id].f, e[id].c);
-            if (d[to] == inf && e[id].c - e[id].f >= lim) {
+            if (d[to] == INF_BFS && e[id].c - e[id].f >= lim) {
                 d[to] = d[cur] + 1;
                 q.push(to);
             }
         }
     }
-    while (!q.empty()) 
+    while (!q.empty())
         q.pop();
-    return d[t] != inf;
+    return d[sink] != INF_BFS;
 }
 
-bool dfs(int v, int flow) {
-    if (flow == 0) 
+bool dfs(int v, ll flow) {
+    if (flow == 0)
         return false;
-    if (v == t) {
-        //cout << v << endl;
+    if (v == sink) {
+        //cerr << v << endl;
         return true;
     }
     for (; pt[v] < g[v].size(); pt[v]++) {
@@ -65,42 +59,29 @@ bool dfs(int v, int flow) {
         int to = e[id].b;
         //printf("v = %d id = %d a = %d b = %d f = %d c = %d\n", v, id, e[id].a, e[id].b, e[id].f, e[id].c);
         if (d[to] == d[v] + 1 && e[id].c - e[id].f >= flow) {
-            int pushed = dfs(to, flow); 
+            bool pushed = dfs(to, flow);
             if (pushed) {
                 e[id].f += flow;
                 e[id ^ 1].f -= flow;
                 return true;
-            }               
+            }
         }
     }
     return false;
 }
 
-void dinic() {
-    for (lim = (1 << 30); lim >= 1;) {
+ll dinic() {
+    ll flow=0;
+    for (lim = (1LL << 62); lim >= 1;) {
         if (!bfs()) {
             lim >>= 1;
             continue;
         }
-        for (int i = s; i <= t; i++) 
-            pt[i] = 0;
-        int pushed;
-        while (pushed = dfs(s, lim)) { 
+        fill(pt, pt + MAXN, 0); // be cautious about this one.
+        while (dfs(source, lim)) {
             flow = flow + lim;
         }
-        //cout << flow << endl;
+        //cerr << flow << endl;
     }
-}
-
-int main() {
-    scanf("%d %d", &n, &m);
-    s = 1; t = n;
-    for (int i = 1; i <= m; i++) {
-        int a, b, c;
-        scanf("%d %d %d", &a, &b, &c);
-        add_edge(a, b, c);
-    }
-    dinic();
-    cout << flow << endl;
-    return 0;
+    return flow;
 }
--- dinic.cpp
+++ dinic.cpp
@@ -1,50 +1,71 @@
 // Dinic: O(V^2*E).
 // Runs in O(E * sqrt(V)) for finding matching in bipartite graph (for more specification go to parsa's talabarg). 
+// For not using long long, make all "ll"s int and change infs. 
 const int maxn = 2e3 + 17, maxm = 5e4 + 17, inf = 1e9;
-int ptr[maxn], head[maxn], prv[maxm], to[maxm], cap[maxm], d[maxn], q[maxn], dis[maxn], so = maxn - 1, sink = maxn - 2, ecnt;
+const int maxn = 2e3 + 17, maxm = 1e4 + 17;
+int ptr[maxn], head[maxn], prv[maxm], to[maxm], d[maxn], q[maxn], dis[maxn], source = maxn - 1, sink = maxn - 2, ecnt;
+ll cap[maxm], inf = (ll)1e17, inf_cap = (ll)1e13;
 void init(){
-	memset(head, -1, sizeof head);
-	ecnt = 0;
+    memset(head, -1, sizeof head);
+    ecnt = 0;
 }
 void add(int v, int u, int vu, int uv = 0){
-	to[ecnt] = u, prv[ecnt] = head[v], cap[ecnt] = vu, head[v] = ecnt++;
-	to[ecnt] = v, prv[ecnt] = head[u], cap[ecnt] = uv, head[u] = ecnt++;
+    to[ecnt] = u, prv[ecnt] = head[v], cap[ecnt] = vu, head[v] = ecnt++;
+    to[ecnt] = v, prv[ecnt] = head[u], cap[ecnt] = uv, head[u] = ecnt++;
 }
 bool bfs(){
-	memset(dis, 63, sizeof dis);
-	dis[so] = 0;
-	int h = 0, t = 0;
-	q[t++] = so;
-	while(h < t){
-		int v = q[h++];
-		for(int e = head[v]; e >= 0; e = prv[e])
-			if(cap[e] && dis[ to[e] ] > dis[v] + 1){
-				dis[ to[e] ] = dis[v] + 1, q[t++] = to[e];
-				if(to[e] == sink)
-					return 1;
-			}
-	}
-	return 0;
+    memset(dis, 63, sizeof dis);
+    dis[source] = 0;
+    int h = 0, t = 0;
+    q[t++] = source;
+    while(h < t){
+        int v = q[h++];
+        for(int e = head[v]; e >= 0; e = prv[e])
+            if(cap[e] && dis[ to[e] ] > dis[v] + 1){
+                dis[ to[e] ] = dis[v] + 1, q[t++] = to[e];
+                if(to[e] == sink)
+                    return 1;
+            }
+    }
+    return 0;
 }
-int dfs(int v, int f = inf){
-	if(v == sink || f == 0)
-		return f;
-	int ret = 0;
-	for(int &e = ptr[v]; e >= 0; e = prv[e])
-		if(dis[v] == dis[ to[e] ] - 1){
-			int x = dfs(to[e], min(f, cap[e]));
-			f -= x, ret += x;
-			cap[e] -= x, cap[e ^ 1] += x;
-			if(!f)
-				break;
-		}
-	return ret;
+ll dfs(int v, ll f = inf){
+    if(v == sink || f == 0)
+        return f;
+    ll ret = 0;
+    for(int &e = ptr[v]; e >= 0; e = prv[e])
+        if(dis[v] == dis[ to[e] ] - 1){
+            ll x = dfs(to[e], min(f, cap[e]));
+            f -= x, ret += x;
+            cap[e] -= x, cap[e ^ 1] += x;
+            if(!f)
+                break;
+        }
+    return ret;
 }
-int mf(){
-	int ans = 0;
-	while(bfs()){
-		memcpy(ptr, head, sizeof ptr);
-		ans += dfs(so);
-	}
-	return ans;
+ll mf(){
+    ll ans = 0;
+    while(bfs()){
+        memcpy(ptr, head, sizeof ptr);
+        ans += dfs(source);
+    }
+    return ans;
+}
+
+// Some of the code of finding cut
+bool visited[maxn];
+void dfs_cut(int v) {
+    visited[v] = true;
+    for (int e = head[v]; e >= 0; e = prv[e]) {
+        if (cap[e] and !visited[to[e]]) {
+            dfs_cut(to[e]);
+        }
+    }
+}
+void find_cut() {
+    cerr << "Left part of cut: ";
+    dfs_cut(source);
+    for(int i = 0; i < maxn; i++)
+        if(visited[i])
+            cerr << i << ' ';
 }
