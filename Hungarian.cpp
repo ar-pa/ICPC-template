2b8b472a7030847baff3021999b7b25ba98dab5f
Parsa Abdollahi <parsa.abdollahi.pa@gmail.com>
2021-09-27 19:38:38 +0330

    Fix Hungarian

--- Hungarian.cpp
+++ Hungarian.cpp
@@ -1,58 +1,45 @@
-typedef long long ll;
-const ll INFL = (1LL << 60);
-using Weight = ll;
-const Weight InfWeight = INFL;
-
-Weight hungarianMin(const vector <vector<Weight>> &A) {
-	if (A.empty()) return 0;
-	int h = A.size(), n = A[0].size();
-	if (h > n) return InfWeight;
-	vector <Weight> fx(h), fy(n);
-	vector<int> x(h, -1), y(n, -1);
-	vector<int> t(n), s(h + 1);
-	for (int i = 0; i < h;) {
-		fill(t.begin(), t.end(), -1);
-		s[0] = i;
-		int q = 0;
-		for (int p = 0; p <= q; ++p) {
-			for (int k = s[p], j = 0; j < n; ++j) {
-				if (fx[k] + fy[j] == A[k][j] && t[j] < 0) {
-					s[++q] = y[j];
-					t[j] = k;
-					if (s[q] < 0) {
-						for (p = j; p >= 0; j = p) {
-							y[j] = k = t[j];
-							p = x[k];
-							x[k] = j;
-						}
-						++i;
-						goto continue_;
-					}
+const ll INFL = (1LL << 50);
+const int N=110; // N is max(up, down)
+ll u[N], v[N], p[N], way[N], minv[N];
+bool used[N];
+pair<vector<ll> , ll> HungarianMin(vector<vector<ll >> &matrix) {
+	ll n = matrix.size(); // matrix must be square.
+	if (n == 0) return {vector<ll> (), 0};
+	for(ll i = 1; i <= n; i++) {
+		for(ll i = 0; i <= n; i++) minv[i] = INFL;
+		memset(way, 0, (n+1) * sizeof(ll ));
+		for(ll j = 0; j <= n; j++) used[j] = false;
+		p[0] = i;
+		ll k0 = 0;
+		do {
+			used[k0] = true;
+			ll i0 = p[k0], k1;
+			ll delta = INFL;
+			for(ll j = 1; j <= n; j++)
+				if(!used[j]) {
+					ll cur = matrix[i0-1][j-1] - u[i0] - v[j];
+					if(cur < minv[j])
+						minv[j] = cur, way[j] = k0;
+					if(minv[j] < delta) 
+						delta = minv[j], k1 = j;
 				}
-			}
-		}
-		if (0) {
-		continue_:;
-		} else {
-			Weight d = InfWeight;
-			for (int j = 0; j < n; j++)
-				if (t[j] < 0) {
-					for (int k = 0; k <= q; ++k)
-						if (A[s[k]][j] != InfWeight)
-							d = min(d, A[s[k]][j] - fx[s[k]] - fy[j]);
-				}
-			if (d == InfWeight)
-				return InfWeight;
-			for (int j = 0; j < n; ++j) {
-				if (t[j] >= 0)
-					fy[j] -= d;
-			}
-			for (int k = 0; k <= q; ++k)
-				fx[s[k]] += d;
-		}
+			for(ll j = 0; j <= n; j++)
+					if(used[j])
+							u[p[j]] += delta, v[j] -= delta;
+					else
+							minv[j] -= delta;
+			k0 = k1;
+		} while (p[k0] != 0);
+		do {
+			ll k1 = way[k0];
+			p[k0] = p[k1];
+			k0 = k1;
+		} while (k0 != 0);
 	}
-	Weight res = 0;
-	for (int i = 0; i < h; ++i)
-		res += A[i][x[i]];
-	return res;
+	vector<ll> ans(n, -1);
+	for(ll j = 1; j <= n; j++) {
+		if(p[j] == 0) continue;
+		ans[p[j] - 1] = j-1;
+	}
+	return {ans, -v[0]};
 }
